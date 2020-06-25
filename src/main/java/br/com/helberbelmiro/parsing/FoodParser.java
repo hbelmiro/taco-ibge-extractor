dcdd960f239c492420a24d252a52b5053521222e
Helber Belmiro <helber.belmiro@gmail.com>
2020-06-25 18:39:04 -0300

    Implemented br.com.helberbelmiro.parsing.FoodParser.parse

--- src/main/java/br/com/helberbelmiro/parsing/FoodParser.java
+++ src/main/java/br/com/helberbelmiro/parsing/FoodParser.java
@@ -19,12 +19,17 @@ public class FoodParser {
         if (line.matches("^[0-9]{7}\\s.*")) {
             return Food.builder()
                        .withCode(parseCode(line))
+                       .withDescription(parseDescription(line))
                        .withPreparation(this.preparationParser.parse(line))
                        .withMacroNutrients(this.macroNutrientsParser.parse(line))
                        .build();
         } else {
             throw new IllegalArgumentException(MessageFormat.format("Invalid food line: [{0}]", line));
         }
+    }
+
+    private String parseDescription(String line) {
+        return line.substring(8, this.preparationParser.getPreparationStartIndex(line) - 1);
     }
 
     private Integer parseCode(String line) {
---
+++ src/test/java/br/com/helberbelmiro/parsing/FoodParserTest.java
@@ -0,0 +1,52 @@
+package br.com.helberbelmiro.parsing;
+
+import br.com.helberbelmiro.model.Food;
+import br.com.helberbelmiro.model.MacroNutrients;
+import br.com.helberbelmiro.model.Preparation;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+class FoodParserTest {
+
+    @Inject
+    FoodParser foodParser;
+
+    private static Stream<Arguments> parseSource() {
+        return Stream.of(
+                Arguments.of(
+                        "6600301 Castanha portuguesa 99 Não se aplica 131,00 2,00 1,38 27,76 4,14",
+                        Food.builder()
+                            .withCode(6600301)
+                            .withDescription("Castanha portuguesa")
+                            .withPreparation(Preparation.builder()
+                                                        .withCode(99)
+                                                        .withDescription("Não se aplica")
+                                                        .build())
+                            .withMacroNutrients(MacroNutrients.builder()
+                                                              .withEnergy(new BigDecimal("131.00"))
+                                                              .withProtein(new BigDecimal("2.00"))
+                                                              .withLipids(new BigDecimal("1.38"))
+                                                              .withCarbs(new BigDecimal("27.76"))
+                                                              .withDietaryFibre(new BigDecimal("4.14"))
+                                                              .build())
+                            .build()
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parseSource")
+    void parse(String line, Food expectedFood) {
+        assertEquals(expectedFood, this.foodParser.parse(line));
+    }
+
+}

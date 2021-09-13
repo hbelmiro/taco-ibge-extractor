1302f2a852c8e26734c5d164331db2edc25ab0f8
hbelmiro <helber.belmiro@gmail.com>
2021-09-13 19:06:36 -0300

    Added TACO pdf as a resource.

--- src/main/java/br/com/helberbelmiro/Extractor.java
+++ src/main/java/br/com/helberbelmiro/Extractor.java
@@ -4,18 +4,21 @@ import br.com.helberbelmiro.reader.PageReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Extractor {
 
-    private static final File FILE = new File("/Users/helber/Desktop/ibge.pdf");
+    private static final File FILE = new File("src/main/resources/META-INF/resources/taco/liv50002.pdf");
+
+    private static final Pattern LINE_BREAK = Pattern.compile("\n\r");
 
     public static void main(String[] args) throws IOException {
         PageReader reader = new PageReader(FILE);
 
         final String pageText = reader.readText(42);
 
-        Stream.of(pageText.split("\n\r"))
+        Stream.of(LINE_BREAK.split(pageText))
               .forEach(System.out::println);
     }
 
